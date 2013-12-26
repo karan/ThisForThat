e85147075508aec74d8b6a15eaba1a0c5ab1e04f
Karan Goel <karan@goel.im>
2013-12-26 14:46:30 -0800

    add more disruptive startup ideas

--- thisforthat.py
+++ thisforthat.py
@@ -45,7 +45,8 @@ THIS = ["Uber", "Skynet", "Digital Music Distribution", "FitBit",
         "News Recommender", "Neural Network", "Tesseract OCR engine",
         "Unreadable CAPTCHA", "Mobile Ecosystem", "Flickr", "Salesforce.com",
         "Twitter Filter", "Wikipedia", "Yelp", "Google App Engine",
-        "Saturday Night Live", "BitTorrent", "Starbucks"]
+        "Saturday Night Live", "BitTorrent", "Starbucks", "Snapchat", "Dropbox,
+        "Pandora", "Cryptocurrency", "Big Data Management Service", "RESTful API"]
 
 THAT = ["Facebook Platform", "Erlang Enthusiasts", "Collegiate Jewish Women",
         "Ex-Girlfriends", "Binders Full of Women", "Mitt Romney's Hair",
